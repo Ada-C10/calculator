699c2464207f09ed9443bf51966f7a7c7a7688cc
Pushpa <pushpaagr108@gmail.com>
2018-08-10 14:16:52 -0700

    Update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -1,56 +1,39 @@
-puts "Hello! Would you like to use the calculator? Enter yes or no"
-cal_use = gets.chomp
-
-while cal_use == "yes"
-
-  puts "Welcome to the Calculator program! Which operator would you like to use\n 1. add(+)\n 2. subtract(-)\n 3. multiply(*)\n 4. divide(/)"
-
-  operator = gets.chomp
-
-  if operator == "+" || operator == "add"
-    puts "pick 1st number"
-    num1 = gets.chomp.to_f
-
-    puts "pick 2nd number"
-    num2 = gets.chomp.to_f
-
-    answer = num1 + num2
-
-  elsif operator == "-" || operator == "subtract"
-    puts "pick 1st number"
-    num1 = gets.chomp.to_f
-
-    puts "pick 2nd number"
-    num2 = gets.chomp.to_f
-
-    answer = num1 - num2
-
-  elsif operator == "*" || operator == "multiply"
-    puts "pick 1st number"
-    num1 = gets.chomp.to_f
-
-    puts "pick 2nd number"
-    num2 = gets.chomp.to_f
-
-    answer = num1 * num2
-
-  elsif operator == "/" || operator == "divide"
-    puts "pick 1st number"
-    num1 = gets.chomp.to_f
-
-    puts "pick 2nd number"
-    num2 = gets.chomp.to_f
-
-    while num2 <= 0
-      puts "please enter a different number for your 2nd number, as it has to be greater than 0"
-      num2 = gets.chomp.to_f
+def operator_method(num1, num2, operator)
+  case operator
+    when "+", "add"
+      puts num1 + num2
+    when "-", "subtract"
+      puts num1 - num2
+    when "*", "multiply"
+      puts num1 * num2
+    when "/", "divide"
+      while num2 <= 0
+        puts "please enter a number greater than 0"
+        num2 = gets.chomp
       end
-      answer = num1 / num2
-  else
-    puts "please only pick from following operators\n 1. add(+)\n 2. subtract(-)\n 3. multiply(*)\n 4. divide(/)"
-    operator = gets.chomp   
+      puts num1 / num2
   end
-  puts answer
-  puts "would you like to play again? enter yes or no"
-  cal_use = gets.chomp
 end
+
+
+def valid_number_user()
+  puts "please enter a valid number"
+  num = gets.chomp
+
+  while num.to_i.to_s != num
+    puts "please enter a valid number"
+    num = gets.chomp
+  end
+
+return num.to_i
+end
+
+puts "Welcome to the Calculator program! Which operator would you like to use\n 1. add(+)\n 2. subtract(-)\n 3. multiply(*)\n 4. divide(/)"
+
+operator = gets.chomp
+
+num1 = valid_number_user()
+num2 = valid_number_user()
+
+operator_method(num1, num2, operator)
+
